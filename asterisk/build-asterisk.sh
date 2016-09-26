8c306542513fc1866812b67a317caa99c1559702
Chad McElligott <chad.mcelligott@gmail.com>
2016-09-26 14:17:43 -0500

    Upgrade to Asterisk 14. New build & config

--- asterisk/build-asterisk.sh
+++ asterisk/build-asterisk.sh
@@ -7,41 +7,53 @@ if test -z ${ASTERISK_VERSION}; then
     exit 1
 fi
 
-# 1.5 jobs per core works out okay
-: ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
+MENUSELECT_DISABLE=(
+    app_talkdetect app_adsiprog app_alarmreceiver app_voicemail
+    app_amd app_chanisavail app_dictate app_externalivr app_festival
+    app_getcpeid app_ices app_image app_minivm app_morsecode app_mp3
+    app_nbscat app_sms app_test app_url app_waitforring app_waitforsilence
+    app_zapateller cdr_custom cdr_manager cdr_syslog cdr_sqlite3_custom
+    cel_custom cel_manager cel_sqlite3_custom chan_iax2 chan_alsa
+    chan_console chan_mgcp chan_oss chan_phone chan_sip chan_skinny
+    chan_unistim func_audiohookinherit pbx_ael pbx_dundi pbx_realtime
+    res_fax res_ael_share res_fax_spandsp res_phoneprov
+    res_pjsip_phoneprov_provider BUILD_NATIVE CORE-SOUNDS-EN-GSM
+)
+
+MENUSELECT_ENABLE=(
+    BETTER_BACKTRACES res_endpoint_stats res_mwi_external res_stasis_mailbox
+    res_ari_mailboxes CORE-SOUNDS-EN-WAV CORE-SOUNDS-EN-ULAW MOH-OPSOUND-WAV
+    MOH-OPSOUND-ULAW EXTRA-SOUNDS-EN-WAV EXTRA-SOUNDS-EN-ULAW
+)
 
 set -ex
 
 mkdir -p /usr/src/asterisk
 cd /usr/src/asterisk
 
-curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz |
+curl -vsL https://github.com/asterisk/asterisk/archive/${ASTERISK_VERSION}.tar.gz |
     tar --strip-components 1 -xz
 
-./configure
+# 1.5 jobs per core works out okay
+: ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
+
+./configure --with-resample --with-pjproject-bundled
 make menuselect/menuselect menuselect-tree menuselect.makeopts
 
-# MOAR SOUNDS
-for i in CORE-SOUNDS-EN MOH-OPSOUND EXTRA-SOUNDS-EN; do
-    for j in ULAW ALAW G722 GSM SLN16; do
-        menuselect/menuselect --enable $i-$j menuselect.makeopts
-    done
+for i in "${MENUSELECT_DISABLE[@]}"; do
+    menuselect/menuselect --disable $i menuselect.makeopts
 done
 
-# ARI mailbox
-menuselect/menuselect --disable app_voicemail menuselect.makeopts
-menuselect/menuselect --enable res_ari_mailboxes menuselect.makeopts
-menuselect/menuselect --enable res_stasis_mailbox menuselect.makeopts
+for i in "${MENUSELECT_ENABLE[@]}"; do
+    menuselect/menuselect --enable $i menuselect.makeopts
+done
 
 make -j ${JOBS} all
 make install
+
 chown -R asterisk:asterisk /var/*/asterisk
 chmod -R 750 /var/spool/asterisk
 mkdir -p /etc/asterisk/
-cp /usr/src/asterisk/configs/basic-pbx/*.conf /etc/asterisk/
-
-# Set runuser and rungroup
-sed -i -E 's/^;(run)(user|group)/\1\2/' /etc/asterisk/asterisk.conf
 
 cd /
 exec rm -rf /usr/src/asterisk
