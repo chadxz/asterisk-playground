54819c965dc46d584b8fe7f7d6fae5d8f2b67c5f
Chad McElligott <chad.mcelligott@gmail.com>
2016-10-28 19:56:05 -0500

    Use local swagger-ui container

---
+++ swagger-ui/swagger-ui-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+PROGNAME=$(basename $0)
+set -e
+
+if test -z ${SWAGGER_URL}; then
+  echo "${PROGNAME}: SWAGGER_URL environment variable required" >&2
+  exit 1
+fi
+
+if test -z ${ARI_USERNAME}; then
+  echo "${PROGNAME}: ARI_USERNAME environment variable required" >&2
+  exit 1
+fi
+
+if test -z ${ARI_PASSWORD}; then
+  echo "${PROGNAME}: ARI_PASSWORD environment variable required" >&2
+  exit 1
+fi
+
+j2 /var/lib/nginx/html/index.html.j2 > /var/lib/nginx/html/index.html
+rm -f /var/lib/nginx/html/index.html.j2
+
+set -x
+exec "$@"
